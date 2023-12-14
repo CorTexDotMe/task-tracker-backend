260e115de61a56e643897592adfd2d920e26ab40
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2023-12-14 20:28:46 +0200

    migration files
    migration to create tables users and tasks

---
+++ internal/database/migrations/0002_task_table_init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS TASKS(
+    taskID INTEGER NOT NULL AUTOINCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    status VARCHAR(255) NOT NULL,
+    done BOOLEAN NOT NULL,
+    dateCreated DATE NOT NULL,
+    dueDate DATE,
+    userID INTEGER NOT NULL,
+    PRIMARY KEY(taskID),
+    FOREIGN KEY(userID) REFERENCES Users(userID)
+)
