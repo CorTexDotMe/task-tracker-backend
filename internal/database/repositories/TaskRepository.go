d0fe38cb26e85010797ba32c5d3a4243b52838af
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2023-12-18 17:50:06 +0200

    Repositories

---
+++ internal/database/repositories/TaskRepository.go
@@ -0,0 +1,38 @@
+package repositories
+
+import (
+	"strconv"
+	"task-tracker-backend/internal/database"
+	"task-tracker-backend/internal/graph/model"
+	"task-tracker-backend/internal/utils"
+)
+
+type TaskRepository struct{}
+
+func (r *TaskRepository) NewTaskToTask(newTask model.NewTask) model.Task {
+	return model.Task{
+		ID:          "",
+		Title:       newTask.Title,
+		Description: newTask.Description,
+		Status:      *newTask.Status,
+		Done:        false,
+		DateCreated: "",
+		DueDate:     nil,
+	}
+}
+
+func (r *TaskRepository) Save(task model.Task) *model.Task {
+	statement, err := database.DATABASE_CONNECTION.Prepare(
+		"INSERT INTO tasks(title,description,status,done,dateCreated,dueDate,userID) VALUES(?,?,?,?,?,?,?)")
+	utils.HandleError(err)
+
+	//TODO add userID
+	res, err := statement.Exec(task.Title, task.Description, task.Status, task.Done, task.DateCreated, task.DueDate)
+	utils.HandleError(err)
+
+	id, err := res.LastInsertId()
+	utils.HandleError(err)
+
+	task.ID = strconv.FormatInt(id, 10)
+	return &task
+}
---
+++ internal/database/repositories/UserRepository.go
@@ -0,0 +1,24 @@
+package repositories
+
+import (
+	"strconv"
+	"task-tracker-backend/internal/database"
+	"task-tracker-backend/internal/graph/model"
+	"task-tracker-backend/internal/utils"
+)
+
+type UserRepository struct{}
+
+func (r *UserRepository) Save(user model.User) model.User {
+	statement, err := database.DATABASE_CONNECTION.Prepare("INSERT INTO users(username,password) VALUES(?,?)")
+	utils.HandleError(err)
+
+	res, err := statement.Exec(user.Name, user.Password)
+	utils.HandleError(err)
+
+	id, err := res.LastInsertId()
+	utils.HandleError(err)
+
+	user.ID = strconv.FormatInt(id, 10)
+	return user
+}
